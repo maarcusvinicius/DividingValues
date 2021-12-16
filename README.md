f5e8198afeac724c4b9293f7a9c639e4cb7bbd67
Marcus Vinicius <marcus.editor77@gmail.com>
2021-12-16 19:34:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Criei um programa que vai ler vários números e colocar em uma lista. Depois disso, cria duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostra o conteúdo das três listas geradas.
+
+--> utilizei 
---
+++ thon.py
@@ -0,0 +1,17 @@
+num = list ()
+pares = list ()
+ímpares = list ()
+while True:
+    num.append(int(input('Digite um número: ')))
+    r = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if r in 'Nn':
+        break
+for i, v in enumerate(num):
+    if v % 2 == 0:
+        pares.append(v)
+    else:
+        ímpares.append(v)
+print('-=' * 15)
+print(f'A lista completa é {num}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de ímpares é {ímpares}')
